61c6bad4e3c4aa64d4dd3fc164292b93c2bd8eec
Mark Probst <mark.probst@gmail.com>
2010-03-27 04:40:01 +0100

    Run more benchmarks.

--- bench.py
+++ bench.py
@@ -2,6 +2,7 @@
 
 import timeit
 import commands
+import sys
 
 def runbench (runs, search, n):
     commands.getstatusoutput ('./searchtest %d %s %d' % (runs, search, n))
@@ -9,14 +10,16 @@ def runbench (runs, search, n):
 def timebench (runs, search, n):
     return timeit.Timer ('runbench (%d, \'%s\', %d)' % (runs, search, n), "from __main__ import runbench").timeit (number = 1)
 
-n = 16
-
-for search in ['linear', 'linear_2', 'linear_4', 'linear_32', 'binary', 'binary_cmov', 'binary_cmov_lin1', 'binary_cmov_lin2', 'binary_cmov_lin4', 'binary_cmov_lin8', 'binary_cmov_lin16', 'binary_cmov_lin32']:
+def timesearch (search, n, num_repeats):
     #print 'Calibrating %s' % search
-    runs = 1000000
+    runs = 10000
     while timebench (runs, search, n) < 2.0:
         runs = runs * 2
     #print '%d runs' % runs
-    for n in [1, 2, 4, 8, 16, 32, 64, 128]:
-        for i in range (10):
-            print '%s %d %d %f' % (search, runs, n, timebench (runs, search, n))
+    for i in range (num_repeats):
+        print '%s %d %d %f' % (search, runs, n, timebench (runs, search, n))
+        sys.stdout.flush ()
+
+for search in ['linear', 'linear_2', 'linear_4', 'linear_42', 'linear_84', 'binary', 'binary_cmov', 'binary_cmov_lin1', 'binary_cmov_lin2', 'binary_cmov_lin4', 'binary_cmov_lin8', 'binary_cmov_lin16', 'binary_cmov_lin32']:
+    for n in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536]:
+        timesearch (search, n, 10)
