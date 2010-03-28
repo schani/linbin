9a00ab59b48d8da785915ef4764e3299a85c85f0
Mark Probst <mark.probst@gmail.com>
2010-03-28 22:03:39 +0200

    Put binary search step into separate header.

--- binary.h
+++ binary.h
@@ -7,18 +7,7 @@ NAME (const int *arr, int n, int key)
 #else
 	while (min < max) {
 #endif
-		int middle = (min + max) >> 1;
-#ifdef CMOV
-		asm ("cmpl %3, %2\n\tcmovg %4, %0\n\tcmovle %5, %1"
-		     : "+r" (min),
-		       "+r" (max)
-		     : "r" (key), "g" (arr [middle]), "g" (middle + 1), "g" (middle));
-#else
-		if (key > arr [middle])
-			min = middle + 1;
-		else
-			max = middle;
-#endif
+#include "binary-step.h"
 	}
 #ifdef LINEAR_FALLBACK
 #if LINEAR_FALLBACK < 4
